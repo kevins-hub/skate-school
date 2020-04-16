0fc28ceb88350646fbb4df22da306c8913659dce
kevins-hub <kevinliu428@gmail.com>
2020-04-15 19:12:23 -0700

    Updated

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,25 +1,44 @@
 <?php
+require_once "Mail.php";
 
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $visitor_email = $_POST['email'];
-    $number = $_POST['number'];
-    $message = $_POST['message'];
-    $type = $_POST['lesson-type'];
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$visitor_email = $_POST['email'];
+$number = $_POST['number'];
+$message = $_POST['message'];
+$type = $_POST['lesson-type'];
 
-    $email_from = "kevinliu428@gmail.com";
-    $email_subject = "New Form Submission";
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Phone Number: $number.\n".
-                            "User lesson preference: $type.\n".
-                                "User Message: $message.\n";
-    
-    $to = "kevinliu428@gmail.com";
+$from = "keepskatingskateschool@gmail.com";
+$to = "kevinliu428@gmail.com";
+$host = "ssl://smtp.gmail.com";
+$port = "465";
+$username = 'keepskatingskateschool@gmail.com';
+$password = 'K3v1nl1uflip428';
 
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
+$email_subject = "New Form Submission";
+$email_body = "User Name: $fname.\n".
+                "User Email: $visitor_email.\n".
+                    "User Phone Number: $number.\n".
+                        "User lesson preference: $type.\n".
+                            "User Message: $message.\n";
 
-    mail($to, $email_subject, $email_body, $headers);
 
-    header("Location: index.html");
+$headers = array ('From' => $from, 'To' => $to,'Subject' => $email_subject);
+$smtp = Mail::factory('smtp',
+array('host' => $host,
+    'port' => $port,
+    'auth' => true,
+    'username' => $username,
+    'password' => $password));
+
+
+$mail = $smtp -> send($to, $headers, $email_body);
+
+
+
+if (PEAR::isError($mail)) {
+    echo($mail->getMessage());
+    } else {
+    echo("Message successfully sent!\n");
+    }
+?>
