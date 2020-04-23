4fdb52f1528f19791278ecfbc16770414e32919c
kevins-hub <kevinliu428@gmail.com>
2020-04-22 21:59:58 -0700

    fixed IG logo js

--- js/app.js
+++ js/app.js
@@ -41,12 +41,13 @@ $(document).ready(function(){
 
 window.onscroll = () => {
   const nav = document.querySelector('#home-nav');
+  const igIcon = document.querySelector('.sm-logo');
   const hlink = document.querySelector('#hp-h-link');
   const llink = document.querySelector('#hp-l-link');
   const alink = document.querySelector('#hp-a-link');
   const clink = document.querySelector('#hp-c-link');
   const mlink = document.querySelector('#hp-m-link');
-  const iglink = document.querySelector('#hp-ig-link');
+  //const iglink = document.querySelector('#hp-ig-link');
   if(this.scrollY <= 10) {
       nav.className = '';
       hlink.className = '';
@@ -54,7 +55,8 @@ window.onscroll = () => {
       alink.className = '';
       clink.className = '';
       mlink.className = '';
-      iglink.className = '';
+      igIcon.style.filter = "invert(100%)";
+      //iglink.className = '';
 
       //navlinks.className = 'hp-links';
   } else{
@@ -64,8 +66,35 @@ window.onscroll = () => {
       alink.className = 'scroll';
       clink.className = 'scroll';
       mlink.className = 'scroll';
-      iglink.className = 'scroll';
+      //iglink.className = 'scroll';
+      igIcon.style.filter = "invert(0%)";
 
       //navlinks.className='op-links';
   }
 };
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function showSlides(n) {
+  console.log(n)
+  var i;
+  slideIndex = n
+  var slides = document.getElementsByClassName("mySlides");
+  //var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+
+      slides[i].style.display = "none";
+      
+  }
+  slides[slideIndex-1].style.display = "block";
+
+}
